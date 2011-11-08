0937532a5d0c5ffe57ec980e1c1c6e089bae3c42
Joerg Huber <joerg.huber@systemic.com.au>
2011-11-08 11:11:38 +0800

    Added comments to class.

--- SBPAgentFramework/sbpframework/src/systemic/sif/sbpframework/publisher/SBPBasePublisher.java
+++ SBPAgentFramework/sbpframework/src/systemic/sif/sbpframework/publisher/SBPBasePublisher.java
@@ -22,6 +22,12 @@ import openadk.library.ElementDef;
 import systemic.sif.sifcommon.publisher.BasePublisher;
 
 /**
+ * <b>Note:</b><p>
+ * Publishers that are part of the SBP must extend this SBPBasePublisher. If your agent deals with other SIF objects 
+ * than the ones defined in the SBP then you should extends the NoneSBPBasePublisher class for these objects and not this
+ * class. If you use this class for any other SIF objects than defined in the SBP the behaviour is unknown, most likely
+ * will cause your agent to crash.
+ *
  * @author Joerg Huber
  *
  */
